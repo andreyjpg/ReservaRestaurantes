adfb294af48f5709da9916207c7c543b54c35d98
maripdelvall <105928181+maripdelvall@users.noreply.github.com>
2022-07-27 19:46:40 -0600

    first test (#11)

--- app/src/main/java/com/example/reservarestaurantes/MainActivity.kt
+++ app/src/main/java/com/example/reservarestaurantes/MainActivity.kt
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private val prueba = 1
+
+
+    private val prueba = null
+    private val prueba2 = 2
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
