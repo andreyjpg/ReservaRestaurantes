63e43c35997123fadba92b19ad7d81a34039617d
Andrey <andreyjpg61@gmail.com>
2022-08-02 13:01:46 -0600

    add restaurant new fragment

---
+++ app/src/main/java/com/example/reservarestaurantes/AddRestaurantFragment.kt
@@ -0,0 +1,21 @@
+package com.example.reservarestaurantes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddRestaurantFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_restaurant, container, false)
+    }
+
+    companion object {
+
+    }
+}
